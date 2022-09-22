b399a991110b7117c3bf2ee94c60ea86c17433c5
KALVEN\user <cjnicdao@csmbataan.ph.education>
2022-09-22 09:06:10 +0800

    Commit2

---
+++ CalvinNicdao.PrelimExam/CalvinNicdao.PrelimExam/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalvinNicdao.PrelimExam.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Index? ViewModel { get; set; }
+        public IActionResult(string? firstName)
+
+        {
+            this.ViewModel = new Index();
+            this ViewModel.FirstName = firstName;
+            return Page();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            var firstName = this.ViewModel?.FirstName;
+            return Page();
+        }
+        
+        
+        {
+        public string? FirstName { get; set; }
+
+       
+        }
+    }
+}
